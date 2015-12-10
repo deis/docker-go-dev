be6d9776c720f2775e286cca767aa7ebedfe260d
Seth Goings <sgoings@engineyard.com>
2015-12-10 14:14:12 -0700

    fix(_scripts): use _USER for less confusion

--- _scripts/ci/deploy.sh
+++ _scripts/ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
+docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 REGISTRY="" make push
-docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -e "$QUAY_EMAIL" -u "$QUAY_USER" -p "$QUAY_PASSWORD" quay.io
 make push
